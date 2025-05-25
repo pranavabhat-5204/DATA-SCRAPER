fd508455cbb8f28f25795307d600d49a9e313b7c
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-05-25 13:39:05 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+#Required imports
 from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-
+#Locating and loading the webdriver
 service = Service("C:/Users/drivers/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
@@ -15,17 +13,17 @@ driver = webdriver.Chrome(service=service)
 url = "https://rera.odisha.gov.in/projects/project-list"
 driver.get(url)
 
-# Wait for table to load
+# Waiting for content to load
 time.sleep(10)
 data=[]
-i=4
+i=0
 while i<6:
     l=[]
 
     time.sleep(10)
-    view_button = driver.find_elements(By.LINK_TEXT, 'View Details')[i]
+    view_button = driver.find_elements(By.LINK_TEXT, 'View Details')[i]  #By find the elements again, we can avoid the issue of stale elements
         
-    driver.execute_script("arguments[0].click();", view_button)
+    driver.execute_script("arguments[0].click();", view_button) #To click on Javascript based elements
     time.sleep(10)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -41,12 +39,14 @@ while i<6:
     details=soup.find_all('div',class_="col-md-6")
     promoter_name=details[0].get_text()
     address=details[5].get_text()
-    gst=details[10].get_text()+details[11].get_text()
+    gst=details[10].get_text()
     l.append(promoter_name.replace('Company Name',''))
     l.append(address.replace('Registered Office Address',''))
     l.append(gst.replace('GST No.',''))
     data.append(l)
     driver.back()
     time.sleep(10)
-    print(data)
+    
     i=i+1
+
+print(data)
